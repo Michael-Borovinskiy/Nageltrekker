b0af000c079e04c9d9e167c12912b0cfc0bd49bc
borovinskiy_me <m.borovinskiy@mail.ru>
2024-09-09 22:04:03 +0300

    added findNearestDates to AnalysisChecks

--- src/main/scala/com/boro/apps/sqlops/AnalysisChecks.scala
+++ src/main/scala/com/boro/apps/sqlops/AnalysisChecks.scala
@@ -1,5 +1,7 @@
 package com.boro.apps.sqlops
 
+import com.boro.apps.sqlops.DateUtils.Period
+import com.boro.apps.sqlops.DateUtils.Period.{Month, Quarter, Year}
 import com.boro.apps.sqlops.DfTransformer._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
@@ -26,12 +28,28 @@ object AnalysisChecks {
       .toDF(namesCols: _*)
   }
 
-  // prepare DF
   def prepareDf(df1: DataFrame, df2: DataFrame, joinColNames: String*): DataFrame = {
 
     renameColsWithPrefSufWithExceptedCols(df1, "", "_", "df1")(joinColNames: _*)
       .join(
         renameColsWithPrefSufWithExceptedCols(df2, "", "_", "df2")(joinColNames: _*)
         , Seq(joinColNames: _*), "full")
+  }
+
+  def findNearestDates(df: DataFrame, columnDt: Column)(period: Period): Seq[String] = {
+
+    val periodCol = period match {
+      case Month => month(columnDt)
+      case Quarter => date_trunc("quarter", columnDt)
+      case Year => year(columnDt)
+      case _ => columnDt
+    }
+
+    df.groupBy(
+      concat(periodCol, lit("_"), year(columnDt)).as("mnth")
+    )
+      .agg(min(columnDt).as(columnDt.toString()))
+      .drop("mnth")
+      .as[String](Encoders.STRING).collect()
   }
 }
